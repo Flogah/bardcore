aa207364d423059b07c56c988fcfd488b663e8f3
Marco <133157182+ProfessorClever@users.noreply.github.com>
2025-10-21 15:51:24 +0200

    Create TestItem

--- bardcore-godot/Components/droppable_item.gd
+++ bardcore-godot/Components/droppable_item.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node3D
 class_name droppable_item
 
 enum item_type {
@@ -10,3 +10,15 @@ enum item_type {
 }
 
 @export var type: item_type = item_type.RING
+@export var modification: int
+@export var mesh: PackedScene
+
+var item_mesh: Node3D
+
+func _ready() -> void:
+	item_mesh = mesh.instantiate()
+	add_child(item_mesh)
+
+func _process(delta: float) -> void:
+	item_mesh.rotate(Vector3.UP, delta * deg_to_rad(25))
+	item_mesh.position.y = lerp(item_mesh.position.y + 1, sin(Time.get_ticks_msec()/300.0)*0.5 + 1, 0.5)
