2190feb013c7fe7f5c077d229c219a4c333226e1
PingKingRing <ludwig.domberger@gmail.com>
2025-11-06 09:58:59 +0100

    t

--- bardcore-godot/ManagerScripts/MusicManager.gd
+++ bardcore-godot/ManagerScripts/MusicManager.gd
@@ -9,7 +9,6 @@ func _ready() -> void:
 	current_music = 0
 	play_music()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
