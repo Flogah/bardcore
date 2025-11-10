2f97e974ed6cc92964fc7d8490eee6e32316344d
Marco <professormarco77@gmail.com>
2025-11-10 13:45:19 +0100

    Create Editorfriendly stat_upgrade resource

---
+++ bardcore-godot/Resources/stat_upgrade_editor_friendly.gd
@@ -0,0 +1,10 @@
+extends stat_upgrade
+
+@export var amount: float
+@export_enum("Multiply", "Add") var operator: int
+
+func apply(stat_value: float) -> float:
+	if operator == 0:
+		return stat_value * amount
+	else:
+		return stat_value + amount
