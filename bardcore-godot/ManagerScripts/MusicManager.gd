beb324c017eebdf2d368c89138cbc96e20873636
PingKingRing <ludwig.domberger@gmail.com>
2025-11-11 16:54:44 +0100

    Music added (wip)

--- bardcore-godot/ManagerScripts/MusicManager.gd
+++ bardcore-godot/ManagerScripts/MusicManager.gd
@@ -1,8 +1,9 @@
 extends Node
 
-var music_list = []
+var music_list: Array
 @export var current_music: AudioStreamPlayer
 var beatTimer: Timer
+var music_volume = -20
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +12,8 @@ func _ready() -> void:
 	beatTimer.start(3.0)
 	current_music = AudioStreamPlayer.new()
 	add_child(current_music)
+	current_music.set_stream(preload("uid://bm46kn62qdwo0"))
+	current_music.volume_db = music_volume
 	current_music.play()
 
 func change_music(index) -> void:
