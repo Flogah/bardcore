ed438a53723b2afaeaeaeb82cac9299396917497
PingKingRing <ludwig.domberger@gmail.com>
2025-11-11 13:45:13 +0100

    t

--- bardcore-godot/ManagerScripts/MusicManager.gd
+++ bardcore-godot/ManagerScripts/MusicManager.gd
@@ -1,8 +1,7 @@
 extends Node
 
 var music_list = []
-var current_music: AudioStreamPlayer
-@export var clips: Node
+@export var current_music: AudioStreamPlayer
 var beatTimer: Timer
 
 # Called when the node enters the scene tree for the first time.
@@ -10,16 +9,13 @@ func _ready() -> void:
 	beatTimer = Timer.new()
 	add_child(beatTimer)
 	beatTimer.start(3.0)
-	play_music("Music")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	current_music = AudioStreamPlayer.new()
+	add_child(current_music)
+	current_music.play()
 
 func change_music(index) -> void:
 	current_music = index
 	#play_music("Music")
 
-func play_music(audio_name: String, from_position: float = 0.0) -> void:
-	current_music = clips.get_node(audio_name)
+func play_music(from_position: float = 0.0) -> void:
 	current_music.play(from_position)
