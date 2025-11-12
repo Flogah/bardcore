72caaa266ca7cb39e562b85fa1f1fbf157448f49
PingKingRing <ludwig.domberger@gmail.com>
2025-11-12 13:28:13 +0100

    BeatManger working

--- bardcore-godot/Instrument/attack_line_area.gd
+++ bardcore-godot/Instrument/attack_line_area.gd
@@ -4,19 +4,21 @@ var beatTimer: Timer
 @onready var hit_emitter_box: hit_emitter_box = $hit_emitter_box
 
 func _ready() -> void:
-	beatTimer = MusicManager.beatTimer
 	#beatTimer = get_tree().get_first_node_in_group('BeatTimer')
 	#unnecessary, it's already connected by hand (check your nodes)
 	#attack_sound.finished.connect(clean_up)
-	beatTimer.timeout.connect(_on_triggered)
+	MusicManager.beat.connect(_on_beat)
 	print("beatTimer")
 
 func _on_triggered() -> void:
 	hit_emitter_box.hit_check()
 	print("EXPLOSION!")
 	attack_sound.play()
-	beatTimer.timeout.disconnect(_on_triggered)
+	MusicManager.beat.disconnect(_on_triggered)
 
 func clean_up() -> void:
 	print("clean up")
 	queue_free()
+
+func _on_beat() -> void:
+	print("Signal beat")
--- bardcore-godot/ManagerScripts/MusicManager.gd
+++ bardcore-godot/ManagerScripts/MusicManager.gd
@@ -4,17 +4,17 @@ var music_list: Array
 @export var current_music: AudioStreamPlayer
 var beatTimer: Timer
 var music_volume = -20
+var music1 = preload("uid://bm46kn62qdwo0")
+@onready var rhythm_notifier: RhythmNotifier = $RhythmNotifier
+signal beat
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	beatTimer = Timer.new()
-	add_child(beatTimer)
-	beatTimer.start(3.0)
-	current_music = AudioStreamPlayer.new()
-	add_child(current_music)
-	current_music.set_stream(preload("uid://bm46kn62qdwo0"))
-	current_music.volume_db = music_volume
-	current_music.play()
+	setup_music()
+	setup_rhythm()
+	setup_timer()
+	rhythm_signal(4)
+	play_music()
 
 func change_music(index) -> void:
 	current_music = index
@@ -22,3 +22,26 @@ func change_music(index) -> void:
 
 func play_music(from_position: float = 0.0) -> void:
 	current_music.play(from_position)
+
+func setup_rhythm() -> void:
+	rhythm_notifier = RhythmNotifier.new()
+	add_child(rhythm_notifier)
+	rhythm_notifier.bpm = 120
+	rhythm_notifier.audio_stream_player = current_music
+
+func setup_timer() -> void:
+	beatTimer = Timer.new()
+	add_child(beatTimer)
+	beatTimer.start(3.0)
+
+func setup_music() -> void:
+	current_music = AudioStreamPlayer.new()
+	add_child(current_music)
+	current_music.set_stream(music1)
+	current_music.volume_db = music_volume
+
+func rhythm_signal(every_beat: int) -> void:
+	rhythm_notifier.beats(every_beat).connect(func(count):
+		print("BEAT"))
+	#emit_signal(notify_signal)
+	beat.emit()
