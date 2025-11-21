4502026217ce2601440a279401d181e493c3c4b1
PingKingRing <ludwig.domberger@gmail.com>
2025-11-21 13:29:28 +0100

    deleted too complicated things

--- bardcore-godot/ManagerScripts/MusicManager.gd
+++ bardcore-godot/ManagerScripts/MusicManager.gd
@@ -1,8 +1,9 @@
 extends Node
 
-var music_list: AudioStreamInteractive
+var music_list: Array
 @export var current_music: AudioStream
-var current_music_player: AudioStreamPlayer
+var music_player_1: AudioStreamPlayer
+var music_player_2: AudioStreamPlayer
 var beatTimer: Timer
 var music_volume = -20
 var music1 = preload("uid://cgp353opwrx4t")
@@ -14,46 +15,46 @@ signal beat
 func _ready() -> void:
 	setup_music_playlist()
 	
-	setup_music_player(current_music)
-	setup_music(0)
+	setup_music_player(music_list[0])
+	setup_music_player(music_list[1])
+	setup_music()
 	setup_rhythm()
 	setup_timer()
 	rhythm_signal(2)
-	play_music()
+	play_music(music_player_1)
 
-func setup_music(index: int) -> void:
-	current_music = AudioStream.new()
-	current_music = music_list.get_clip_stream(index)
+func setup_music() -> void:
+	pass
 
 func change_music(index: int) -> void:
-	music_list.switch_to_clip(index)
+	pass
 
-func play_music(from_position: float = 0.0) -> void:
-	current_music_player.play(from_position)
+func play_music(audiostreamplayer: AudioStreamPlayer) -> void:
+	audiostreamplayer.play()
 
 func setup_rhythm() -> void:
 	rhythm_notifier = RhythmNotifier.new()
 	add_child(rhythm_notifier)
 	rhythm_notifier.bpm = 120
-	rhythm_notifier.audio_stream_player = current_music_player
+	rhythm_notifier.audio_stream_player = music_player_1
 
 func setup_timer() -> void:
 	beatTimer = Timer.new()
 	add_child(beatTimer)
 	beatTimer.start(3.0)
 
-func setup_music_player(current_music: AudioStream) -> void:
-	current_music_player = AudioStreamPlayer.new()
-	add_child(current_music_player)
-	current_music_player.set_stream(music_list)
-	current_music_player.set_autoplay(true)
-	current_music_player.volume_db = music_volume
+func setup_music_player(asp: AudioStreamPlayer) -> void:
+	asp = AudioStreamPlayer.new()
+	add_child(asp)
+	asp.set_autoplay(true)
+	asp.volume_db = music_volume
+	music_player_1.set_stream(music1)
+	music_player_2.set_stream(music2)
 
 func setup_music_playlist() -> void:
-	music_list = AudioStreamInteractive.new()
-	music_list.set_clip_stream(0, music1)
-	music_list.set_clip_stream(1, music2)
-	music_list.set_initial_clip(0)
+	music_list.resize(2)
+	music_list.insert(0, music_player_1)
+	music_list.insert(1, music_player_2)
 
 func rhythm_signal(every_beat: int) -> void:
 	rhythm_notifier.beats(every_beat).connect(func(count):
