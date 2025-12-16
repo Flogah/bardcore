59faf9a528f865f0bb95418ada261160d71ef837
Marco <133157182+ProfessorClever@users.noreply.github.com>
2025-12-16 18:13:42 +0100

    connect slot size change to inventory drop

--- bardcore-godot/Components/inventory_component.gd
+++ bardcore-godot/Components/inventory_component.gd
@@ -22,15 +22,16 @@ const SLOT_TYPE_SIZE := {
 	droppable_item.item_type.RING: stat_component.stat_id.RING_SLOTS,
 }
 
+func _ready() -> void:
+	await get_tree().create_timer(0.1).timeout
+	for type in SLOT_TYPE_SIZE.keys():
+		stat_comp.stats[type].changed.connect(drop_overflowing_itmes)
+
 func pickup(item: droppable_item) -> void: #call this if a item should be forced into a slot
-	var slot = slots[item.item_resource_.type]
-	if item.item_resource_.type == item.item_type.INSTRUMENT:
-		drop(item.item_resource_.type)
-	elif slot.size() >= int(stat_comp.get_stat(SLOT_TYPE_SIZE[item.item_resource_.type])):
-		drop(item.item_resource_.type)
 	slots[item.item_resource_.type].append(item)
 	stat_comp.add_upgrades(item.get_instance_id(),item.item_resource_.upgrades)
 	item.get_parent().remove_child(item)
+	drop_overflowing_itmes(item.item_resource_.type)
 	
 
 func drop(item_type: droppable_item.item_type) -> void:
@@ -44,3 +45,13 @@ func drop(item_type: droppable_item.item_type) -> void:
 func drop_all():
 	for type in droppable_item.item_type:
 		drop(type)
+
+func drop_overflowing_itmes(item_type: droppable_item.item_type) -> void:
+	var slot = slots[item_type]
+	if item_type == droppable_item.item_type.INSTRUMENT:
+		if slot.size() > 1:
+			drop(item_type)
+			return
+	while slot.size() > int(stat_comp.get_stat(SLOT_TYPE_SIZE[item_type])):
+		drop(item_type)
+	return
