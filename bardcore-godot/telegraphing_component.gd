72cc3aec72036c2bb9e0489d6b67528cf6528ecd
Marco <133157182+ProfessorClever@users.noreply.github.com>
2025-12-18 22:05:21 +0100

    refactor telegrapher

--- bardcore-godot/telegraphing_component.gd
+++ bardcore-godot/telegraphing_component.gd
@@ -2,7 +2,6 @@ extends Node3D
 class_name telegraphing_component
 
 var e_type = hit_effect.effect_type
-var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 func display_number(value: int, type: hit_effect.effect_type):
 	var num: Label3D = Label3D.new()
@@ -23,8 +22,8 @@ func display_number(value: int, type: hit_effect.effect_type):
 	
 	owner.owner.add_child(num)
 	num.global_position = global_position
-	num.position.x += rng.randf_range(-2,2)
-	num.position.y += rng.randf_range(-2,2)
+	num.position.x += randf_range(-2,2)
+	num.position.y += randf_range(-2,2)
 	
 	var tween: Tween = get_tree().create_tween()
 	
@@ -32,6 +31,4 @@ func display_number(value: int, type: hit_effect.effect_type):
 	tween.tween_property(num, "position:y", num.position.y - 24, 0.25).set_ease(Tween.EASE_OUT)
 	tween.tween_property(num, "position:y", num.position.y, 0.5).set_ease(Tween.EASE_IN).set_delay(0.25)
 	tween.tween_property(num, "scale", Vector3.ZERO, 0.25).set_delay(0.5).set_ease(Tween.EASE_IN)
-	
-	await tween.finished
-	num.queue_free()
+	tween.finished.connect(num.queue_free)
