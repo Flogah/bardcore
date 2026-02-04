a690bc804638da9e2ede1f8eb07dd86870edc8fd
Flogah <89162350+Flogah@users.noreply.github.com>
2026-02-04 21:25:34 +0100

    added better enemy death state

--- bardcore-godot/Enemy/enemy.gd
+++ bardcore-godot/Enemy/enemy.gd
@@ -1,6 +1,8 @@
 class_name Enemy
 extends CharacterBody3D
 
+signal died
+
 var gravity:float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var blood_particles: GPUParticles3D = $BloodParticles
 @onready var visual: Node3D = $Visual
@@ -8,24 +10,17 @@ var gravity:float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @export var move_speed: float = 2
 
-var dead: bool = false
-
 func _physics_process(delta: float) -> void:
-	if dead:
-		return
-	
 	if visual.scale.y < 1.0:
 		visual.scale.y += .02
 	
-	velocity.y -= gravity * delta
 	move_and_slide()
 
 func _on_health_component_died() -> void:
-	dead = true
-	get_parent().remove_child(self)
-	queue_free()
+	var statemachine = $StateMachine
+	statemachine._transition_to_next_state("EnemyDead")
 
-func bleed():
+func bleed(_amount, _mod_amount, _total):
 	blood_particles.restart()
 	squish()
 
