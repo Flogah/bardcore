d5f746672187dc46900efcd984ff870303225945
Flogah <89162350+Flogah@users.noreply.github.com>
2026-02-04 17:18:08 +0100

    hid treasure; gave enemy attacks and animations; added hit receive box to players

---
+++ bardcore-godot/States/Enemy/EnemyAttack.gd
@@ -0,0 +1,19 @@
+extends State
+class_name EnemyAttack
+
+@export var animation_player: AnimationPlayer
+@export var attack_area: hit_emitter_box
+
+func enter(previous_state_path: String, data := {}) -> void:
+	animation_player.play("attack")
+	attack_area.hit_check()
+	
+	var attack_timer = Timer.new()
+	attack_timer.timeout.connect(end_attack)
+	attack_timer.one_shot = true
+	attack_timer.autostart = false
+	add_child(attack_timer)
+	attack_timer.start(.3)
+
+func end_attack():
+	finished.emit("EnemyFollow")
---
+++ bardcore-godot/States/Enemy/Threaten_Attack.gd
@@ -0,0 +1,19 @@
+extends State
+class_name ThreatenAttack
+
+@export var animation_player: AnimationPlayer
+@export var threatening_time: float = .5
+
+func enter(previous_state_path: String, data := {}) -> void:
+	animation_player.play("threaten")
+	
+	var threat_timer = Timer.new()
+	threat_timer.timeout.connect(end_threat)
+	threat_timer.one_shot = true
+	threat_timer.autostart = false
+	add_child(threat_timer)
+	threat_timer.start(threatening_time)
+
+func end_threat():
+	animation_player.stop()
+	finished.emit("EnemyAttack")
