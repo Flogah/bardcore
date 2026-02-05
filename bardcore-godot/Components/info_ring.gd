b1151f11416236a3f483bc339a8bfde45ad5fabc
Marco <133157182+ProfessorClever@users.noreply.github.com>
2026-02-05 21:54:54 +0100

    Make healthbar shader unique for each player

--- bardcore-godot/Components/info_ring.gd
+++ bardcore-godot/Components/info_ring.gd
@@ -5,13 +5,15 @@ var health_comp: health_component
 
 func _ready():
 	await get_tree().create_timer(.5).timeout
-	material = torus.get_active_material(0) as ShaderMaterial
+	var original = torus.get_active_material(0)
+	
+	material = original.duplicate(true) as ShaderMaterial
+	torus.set_surface_override_material(0,material)
 
 	health_comp = get_parent().get_node("HealthComponent")
 
 	health_comp.damaged.connect(_on_health_changed)
 	health_comp.healed.connect(_on_health_changed)
-	health_comp.stat_comp.get_stat_object(stat_component.stat_id.MAX_HEALTH).changed.connect(_update_ring)
 
 	_update_ring(health_comp.health)
 
