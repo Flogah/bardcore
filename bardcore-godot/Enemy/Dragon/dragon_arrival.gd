e4b14aa3c70dce9ea2f251c134656053e0759596
Flogah <89162350+Flogah@users.noreply.github.com>
2026-02-05 19:05:05 +0100

    animated shaking

--- bardcore-godot/Enemy/Dragon/dragon_arrival.gd
+++ bardcore-godot/Enemy/Dragon/dragon_arrival.gd
@@ -1,7 +1,11 @@
 extends Node3D
 
+
 func small_shake():
 	MapManager.current_map.shake(0.3, 3.0, 0.2)
 
 func big_shake():
 	MapManager.current_map.shake(0.6, 3.0, 0.5)
+
+func game_over():
+	GameManager.dragon_death()
