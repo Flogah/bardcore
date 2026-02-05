a46167a28761e037ed7a178d946e9c6089b5ad40
Flogah <89162350+Flogah@users.noreply.github.com>
2026-02-05 17:44:52 +0100

    trying to fix movement bug

--- bardcore-godot/States/Player/Movement/PlayerRunning.gd
+++ bardcore-godot/States/Player/Movement/PlayerRunning.gd
@@ -6,14 +6,13 @@ func enter(previous_state_path: String, data := {}) -> void:
 	pass
 
 func physics_update(delta: float) -> void:
-	if !owner.can_move:
-		return
-	
 	var input_dir = input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	if input_dir:
 		player.velocity.x = input_dir.x * player.stat_comp.get_stat(stat_component.stat_id.MOVEMENT_SPEED)
 		player.velocity.z = input_dir.y * player.stat_comp.get_stat(stat_component.stat_id.MOVEMENT_SPEED)
+	elif !owner.can_move:
+		player.velocity = Vector3.ZERO
 	else:
 		finished.emit(IDLE)
 	
