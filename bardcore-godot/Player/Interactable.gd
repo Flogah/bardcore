18e0b2c1563217076fc1dca86900ffb53f94a859
Marco <133157182+ProfessorClever@users.noreply.github.com>
2026-02-14 16:07:46 +0100

    Make interactable child of Area3D

--- bardcore-godot/Player/Interactable.gd
+++ bardcore-godot/Player/Interactable.gd
@@ -1,7 +1,29 @@
 class_name Interactable
-extends Node3D
+extends Area3D
 
-var hint:InputHint
+var hint: InputHint
+var closest_hands: Array[Area3D]
 
-func interact():
+func _ready() -> void:
+	# Only exist in interactable layer and no mask:
+	set_collision_layer_value(1, false)
+	set_collision_layer_value(4, true)
+	set_collision_mask_value(1, false)
+
+func interact() -> void:
 	pass
+
+func display_hint() -> void:
+	pass
+
+func remove_hint() -> void:
+	pass
+
+func append_player_hand_beeing_closest(hand: Area3D) -> void:
+	closest_hands.append(hand)
+	display_hint()
+
+func remove_player_hand_beeing_closest(hand: Area3D) -> void:
+	closest_hands.erase(hand)
+	if closest_hands.size() < 1:
+		remove_hint()
