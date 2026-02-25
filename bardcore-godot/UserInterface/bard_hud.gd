d949a7030be9633eb35abbb6e1fb2a36a530bb86
Flogah <89162350+Flogah@users.noreply.github.com>
2026-02-25 17:01:39 +0100

    made health bars now functional

--- bardcore-godot/UserInterface/bard_hud.gd
+++ bardcore-godot/UserInterface/bard_hud.gd
@@ -15,6 +15,12 @@ func setup_HUD(player: int):
 	player_num = player
 	set_type()
 	PlayerManager.player_data_updated.connect(set_type)
+	var player_node = PlayerManager.player_nodes[player_num]
+	var health_comp: health_component = player_node.health_comp
+	health_comp.damaged.connect(update_health)
+	health_comp.healed.connect(update_health)
+	update_health(0, 0, health_comp.health)
+	set_max_health(health_comp.max_health)
 
 func set_type():
 	current_type = PlayerManager.player_data[player_num]["bard"]
@@ -25,8 +31,8 @@ func set_type():
 	else:
 		portrait.texture = GODOT_ICON
 
-func update_health(new_value: float):
-	health_bar.value = new_value
+func update_health(_amount, _mod, new_h: float):
+	health_bar.value = new_h
 
 func set_max_health(new_value: float):
 	health_bar.max_value = new_value
