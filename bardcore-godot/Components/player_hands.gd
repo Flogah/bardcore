854c52382a2263ea42073067444fc664275cd0ba
Marco <133157182+ProfessorClever@users.noreply.github.com>
2026-03-02 22:59:08 +0100

    reimplement hint show closest

--- bardcore-godot/Components/player_hands.gd
+++ bardcore-godot/Components/player_hands.gd
@@ -2,26 +2,27 @@ extends Area3D
 class_name PlayerHands
 
 var closest_interactable: Interactable = null
-@onready var check_timer: Timer = $"Check Timer"
+@export var check_timer: Timer
 
 func _on_area_entered(area: Area3D) -> void:
 	if get_overlapping_areas().size() == 1:
-		closest_interactable = area
-		closest_interactable.append_player_hand_beeing_closest(self)
+		set_hands_closest(area)
 	else:
 		check_closest()
-		check_timer.start()
+		if check_timer.is_stopped(): check_timer.start()
 
 func _on_area_exited(area: Area3D) -> void:
 	var overlapping_areas_size: int = get_overlapping_areas().size()
 	area.remove_player_hand_beeing_closest(self)
-	if overlapping_areas_size < 1:
+	if overlapping_areas_size <= 0:
 		check_timer.stop()
 		closest_interactable.remove_player_hand_beeing_closest(self)
 		closest_interactable = null
 	elif overlapping_areas_size == 1:
 		check_timer.stop()
-		closest_interactable = get_overlapping_areas()[0]
+		set_hands_closest(get_overlapping_areas()[0])
+	elif overlapping_areas_size != 0:
+		check_closest()
 
 func check_closest() -> void:
 	var closest: Interactable = get_overlapping_areas()[0]
@@ -31,7 +32,11 @@ func check_closest() -> void:
 		if distance_to_area < closest_distance:
 			closest = area as Interactable
 			closest_distance = distance_to_area
+	set_hands_closest(closest)
+
+func set_hands_closest(closest: Interactable) -> void:
 	if closest != closest_interactable:
-		closest_interactable.remove_player_hand_beeing_closest(self)
+		if closest_interactable:
+			closest_interactable.remove_player_hand_beeing_closest(self)
 		closest_interactable = closest
 		closest_interactable.append_player_hand_beeing_closest(self)
