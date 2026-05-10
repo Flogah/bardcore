22d05d436c9d3d4dfc38b9942b113f5135766357
Marco <133157182+ProfessorClever@users.noreply.github.com>
2026-05-10 16:13:02 +0200

    tonart fix

--- midi-experiment-bpm/Schnittstelle.gd
+++ midi-experiment-bpm/Schnittstelle.gd
@@ -7,11 +7,10 @@ signal water
 signal day
 signal night
 
-var notes_played: Array = [0,0,0,0,0]
-var tonart: int = 0
+var notes_played: Array = [{"note": 0, "kind": 0},{"note": 0, "kind": 0},{"note": 0, "kind": 0},{"note": 0, "kind": 0},]
 
-func add_note(note: int) -> void:
-	notes_played.push_front(note)
+func add_note(note: int, kind: int) -> void:
+	notes_played.push_front({"note": note, "kind": kind})
 	if notes_played.size() > 5:
 		notes_played = notes_played.slice(0,4)
 	check_for_triggers()
@@ -22,7 +21,7 @@ func check_for_triggers() -> void:
 	check_night_or_day()
 
 func check_up_or_down() -> void:
-	var difference: int = notes_played[0] - notes_played[1]
+	var difference: int = notes_played[0]["note"] - notes_played[1]["note"]
 	if difference > 0:
 		up.emit()
 	elif difference < 0:
@@ -30,13 +29,13 @@ func check_up_or_down() -> void:
 	print(difference)
 
 func check_fire_or_water() -> void:
-	if notes_played[0] == notes_played[2] && notes_played[1] == notes_played[3]:
+	if notes_played[0]["note"] == notes_played[2]["note"] && notes_played[1]["note"] == notes_played[3]["note"]:
 		fire.emit()
-	elif notes_played[0] == notes_played[1] && notes_played[1] == notes_played[2] && notes_played[2] == notes_played[3]:
+	elif notes_played[0]["note"] == notes_played[1]["note"] && notes_played[1]["note"] == notes_played[2]["note"] && notes_played[2]["note"] == notes_played[3]["note"]:
 		water.emit()
 
 func check_night_or_day() -> void:
-	if tonart == 0:
+	if notes_played[0]["kind"] == 0:
 		day.emit()
-	if tonart == -1:
+	if notes_played[0]["kind"] == -1:
 		night.emit()
