150e4547f04dbb31497fce9c547aec3ab550cf5c
Mohito <89162350+Flogah@users.noreply.github.com>
2026-05-13 14:40:49 +0200

    added an end "portal" and a simple way to reset after reaching the end

--- midi-experiment-bpm/Scenes/LevelParts/end_portal.gd
+++ midi-experiment-bpm/Scenes/LevelParts/end_portal.gd
@@ -2,14 +2,11 @@ class_name EndPortal
 extends Area2D
 
 signal end_reached
-@onready var game_over_ui: CanvasLayer = %GameOverUI
 
 func _on_body_entered(body: Node2D) -> void:
 	if !body.is_in_group("playerpuppet"):
 		return
-	
-	end_reached.emit()
+	_on_end_reached()
 	
 func _on_end_reached():
-	get_tree().paused = true
-	game_over_ui.show()
+	end_reached.emit()
--- midi-experiment-bpm/Scenes/Testlevel_Stage/cool_lvl.gd
+++ midi-experiment-bpm/Scenes/Testlevel_Stage/cool_lvl.gd
@@ -7,9 +7,29 @@ var day: bool = false
 @onready var ui_day: Sprite2D = $MusikbarTag
 @onready var ui_night: Sprite2D = $MusikbarNacht
 
+var game_over:bool = false
+
+@export var end_portal:EndPortal
+@export var game_over_ui:CanvasLayer
+
 func _ready() -> void:
 	Schnittstelle.day.connect(switch_day_night.bind(true))
 	Schnittstelle.night.connect(switch_day_night.bind(false))
+	end_portal.end_reached.connect(_on_game_over_reached)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("reset"):
+		manual_reset()
+
+func _on_game_over_reached():
+	game_over = true
+	game_over_ui.show()
+	get_tree().paused = true
+
+func manual_reset():
+	get_tree().reload_current_scene()
+
+
 
 func switch_day_night(day: bool) -> void:
 	stage_day.visible = day
