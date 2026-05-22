1793a63581628983190f163ae7ed8e3d72a66c2f
Mohito <89162350+Flogah@users.noreply.github.com>
2026-05-22 17:06:31 +0200

    added video and automatic reload

---
+++ bardcore-godot/Menus/demo_end_screen.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var timer: Timer = %Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	UserInterface.hide()
+	timer.timeout.connect(_on_timeout)
+	timer.start()
+
+func _on_timeout():
+	UserInterface.show()
+	get_tree().change_scene_to_packed(load("uid://7sn5taw7aace"))
