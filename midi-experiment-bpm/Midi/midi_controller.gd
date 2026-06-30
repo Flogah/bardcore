ecb1cbaad0b1fc59048ecedde9d5dc647e7a8417
Mohito <89162350+Flogah@users.noreply.github.com>
2026-06-30 21:39:16 +0200

    commented out extra functions

--- midi-experiment-bpm/Midi/midi_controller.gd
+++ midi-experiment-bpm/Midi/midi_controller.gd
@@ -73,9 +73,9 @@ var time_signature: int = 0 # jetzt BEAT-basiert (0–3)
 
 func _ready() -> void:
 	init_amy()
-	achtel.connect(play_note)
-	notes = get_octave()
-	update_bpm(bpm)
+	#achtel.connect(play_note)
+	#notes = get_octave()
+	#update_bpm(bpm)
 	
 	current_stringing = stringings["g_stimmung"]
 
@@ -84,7 +84,7 @@ func _process(delta: float) -> void:
 	handle_input()
 	
 	#playback_from_capture()
-	read_note_queue(delta)
+	#read_note_queue(delta)
 	
 	if debug_mode:
 		update_debug_ui()
@@ -96,19 +96,19 @@ func init_amy():
 
 # ------------------- BPM -------------------
 
-func update_bpm(new_bpm: float):
-	var old_bpm = bpm
-	bpm = clamp(new_bpm, 20.0, 300.0)
-	if bpm != old_bpm:
-		bpm_changed.emit(bpm)
-	seconds_per_beat = 60.0 / bpm
-	seconds_per_achtel = seconds_per_beat / 2.0
-
-func increase_bpm(amount: float = 5.0):
-	update_bpm(bpm + amount)
-
-func decrease_bpm(amount: float = 5.0):
-	update_bpm(bpm - amount)
+#func update_bpm(new_bpm: float):
+	#var old_bpm = bpm
+	#bpm = clamp(new_bpm, 20.0, 300.0)
+	#if bpm != old_bpm:
+		#bpm_changed.emit(bpm)
+	#seconds_per_beat = 60.0 / bpm
+	#seconds_per_achtel = seconds_per_beat / 2.0
+#
+#func increase_bpm(amount: float = 5.0):
+	#update_bpm(bpm + amount)
+#
+#func decrease_bpm(amount: float = 5.0):
+	#update_bpm(bpm - amount)
 
 # ------------------- TIMING -------------------
 
@@ -135,10 +135,10 @@ func handle_timing(delta: float):
 # ------------------- INPUT -------------------
 
 func handle_input():
-	if Input.is_action_just_pressed("bpm_up"):
-		increase_bpm()
-	if Input.is_action_just_pressed("bpm_down"):
-		decrease_bpm()
+	#if Input.is_action_just_pressed("bpm_up"):
+		#increase_bpm()
+	#if Input.is_action_just_pressed("bpm_down"):
+		#decrease_bpm()
 	
 	if Input.is_action_just_pressed("pick_1"):
 		pluck(1)
@@ -171,10 +171,10 @@ func handle_input():
 			tonart = 0
 			tonart_changed.emit("dur")
 	
-	if Input.is_action_just_pressed("capture_mode"):
-		capture_mode_activation(true)
-	if Input.is_action_just_released("capture_mode"):
-		capture_mode_activation(false)
+	#if Input.is_action_just_pressed("capture_mode"):
+		#capture_mode_activation(true)
+	#if Input.is_action_just_released("capture_mode"):
+		#capture_mode_activation(false)
 
 
 # ------------------- NOTE SYSTEM -------------------
@@ -184,46 +184,46 @@ func pluck(string_num:int):
 	
 	amy.send({"synth": string_num, "patch": patch, "num_voices": 6, "note": note, "vel": 1.0})
 
-func queue_note(data: Dictionary, delay:float = 0.0):
-	var synth = data["synth"]
-	var note = data["note"]
-	var vel = data["vel"]
-	var kind = data["kind"]
-	
-	note_queue[delay] = {"synth": synth, "patch": patch, "num_voices": 6, "note": note, "vel": vel, "kind": kind}
-	
-	if capture_mode:
-		capture_note(data)
+#func queue_note(data: Dictionary, delay:float = 0.0):
+	#var synth = data["synth"]
+	#var note = data["note"]
+	#var vel = data["vel"]
+	#var kind = data["kind"]
+	#
+	#note_queue[delay] = {"synth": synth, "patch": patch, "num_voices": 6, "note": note, "vel": vel, "kind": kind}
+	#
+	#if capture_mode:
+		#capture_note(data)
 
-func read_note_queue(delta):
-	var keys = note_queue.keys()
-	for timing in keys:
-		var data = note_queue[timing]
-		note_queue.erase(timing)
-		var new_timing = timing
-		new_timing -= delta
-		if new_timing <= 0.0:
-			play_note_direct(data)
-		else:
-			note_queue[new_timing] = data
+#func read_note_queue(delta):
+	#var keys = note_queue.keys()
+	#for timing in keys:
+		#var data = note_queue[timing]
+		#note_queue.erase(timing)
+		#var new_timing = timing
+		#new_timing -= delta
+		#if new_timing <= 0.0:
+			#play_note_direct(data)
+		#else:
+			#note_queue[new_timing] = data
 
-func play_note():
-	if waiting_note.size() > 0:
-		amy.send({"synth": waiting_note["synth"], "patch": waiting_note["patch"], "num_voices": 6, "note": waiting_note["note"] + waiting_note["kind"], "vel": waiting_note["vel"]})
-		Schnittstelle.add_note(waiting_note["note"], waiting_note["kind"])
-		last_played_note = waiting_note["note"]
-		note_played.emit(waiting_note["note"])
-		waiting_note = {}
-
-func play_note_direct(data: Dictionary):
-	var synth = data["synth"]
-	var note = data["note"]
-	var vel = data["vel"]
-	var kind = data["kind"]
-	
-	amy.send({"synth": synth, "patch": patch, "num_voices": 6, "note": note + kind, "vel": vel})
-	Schnittstelle.add_note(note, kind)
-	note_played.emit(note)
+#func play_note():
+	#if waiting_note.size() > 0:
+		#amy.send({"synth": waiting_note["synth"], "patch": waiting_note["patch"], "num_voices": 6, "note": waiting_note["note"] + waiting_note["kind"], "vel": waiting_note["vel"]})
+		#Schnittstelle.add_note(waiting_note["note"], waiting_note["kind"])
+		#last_played_note = waiting_note["note"]
+		#note_played.emit(waiting_note["note"])
+		#waiting_note = {}
+#
+#func play_note_direct(data: Dictionary):
+	#var synth = data["synth"]
+	#var note = data["note"]
+	#var vel = data["vel"]
+	#var kind = data["kind"]
+	#
+	#amy.send({"synth": synth, "patch": patch, "num_voices": 6, "note": note + kind, "vel": vel})
+	#Schnittstelle.add_note(note, kind)
+	#note_played.emit(note)
 
 #func strum_up(strum_delay:float = 0.07):
 	#queue_note(pack_note(1, note1+pitch), strum_delay * 0)
@@ -241,67 +241,67 @@ func play_note_direct(data: Dictionary):
 
 # ------------------- CAPTURE -------------------
 
-func capture_note(data: Dictionary):
-	data["synth"] = 2
-	
-	var key = str(time_signature) + "_" + str(achtel_count % 8)
-	capture_dict_1[key] = data
+#func capture_note(data: Dictionary):
+	#data["synth"] = 2
+	#
+	#var key = str(time_signature) + "_" + str(achtel_count % 8)
+	#capture_dict_1[key] = data
+#
+#func playback_from_capture():
+	#var key = str(time_signature) + "_" + str(achtel_count % 8)
+	#
+	##if capture_dict_1.has(key):
+		##queue_note(capture_dict_1[key])
 
-func playback_from_capture():
-	var key = str(time_signature) + "_" + str(achtel_count % 8)
-	
-	#if capture_dict_1.has(key):
-		#queue_note(capture_dict_1[key])
-
-func capture_mode_activation(toggle):
-	if toggle:
-		capture_dict_1 = {}
-		capture_mode = true
-	else:
-		capture_mode = false
-	capture_mode_changed.emit(capture_mode)
+#func capture_mode_activation(toggle):
+	#if toggle:
+		#capture_dict_1 = {}
+		#capture_mode = true
+	#else:
+		#capture_mode = false
+	#capture_mode_changed.emit(capture_mode)
 
 # ------------------- MISC -------------------
 
-func get_octave(start_note: int = starting_note) -> Array:
-	return [
-		0,
-		start_note,
-		start_note +2,
-		start_note +4,
-		start_note +8,
-		start_note +10,
-		start_note +12,
-		start_note +14,
-		start_note +16,
-	]
+#func get_octave(start_note: int = starting_note) -> Array:
+	#return [
+		#0,
+		#start_note,
+		#start_note +2,
+		#start_note +4,
+		#start_note +8,
+		#start_note +10,
+		#start_note +12,
+		#start_note +14,
+		#start_note +16,
+	#]
 
-func apply_scale(note_array: Array, scale: String) -> Array:
-	var new_array = note_array
-	match scale:
-		"d-moll":
-			for note in note_array:
-				if note % 12 == 11:
-					note -= 1
-			new_array = note_array.map(func(n): return n + 2)
-		"f-moll":
-			for note in note_array:
-				var r = note % 12
-				if r == 4 or r == 9 or r == 11:
-					note -= 1
-			new_array = note_array.map(func(n): return n + 5)
-		"test":
-			pass
-	scale_changed.emit(scale)
-	return new_array
+#func apply_scale(note_array: Array, scale: String) -> Array:
+	#var new_array = note_array
+	#match scale:
+		#"d-moll":
+			#for note in note_array:
+				#if note % 12 == 11:
+					#note -= 1
+			#new_array = note_array.map(func(n): return n + 2)
+		#"f-moll":
+			#for note in note_array:
+				#var r = note % 12
+				#if r == 4 or r == 9 or r == 11:
+					#note -= 1
+			#new_array = note_array.map(func(n): return n + 5)
+		#"test":
+			#pass
+	#scale_changed.emit(scale)
+	#return new_array
 
-func set_tonart(dur: bool) -> void:
-	if dur:
-		tonart = 0
-		tonart_changed.emit("dur")
-	else:
-		tonart = -1
-		tonart_changed.emit("moll")
+#func set_tonart(dur: bool) -> void:
+	#if dur:
+		#tonart = 0
+		#tonart_changed.emit("dur")
+	#else:
+		#tonart = -1
+		#tonart_changed.emit("moll")
 
 func get_note_name(note: int) -> String:
 	var note_name: String = "NaN"
